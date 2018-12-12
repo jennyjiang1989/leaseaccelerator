b0e62e90069373254d1ed61d41292b3042429e2b
Jenny Jiang <jiangchunxing@gmail.com>
2018-12-12 14:28:17 -0800

    add requested inputs outputs

--- perpetualTokenClient.java
+++ perpetualTokenClient.java
@@ -28,13 +28,28 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.entity.mime.content.ByteArrayBody;//newly added
-//import org.json.simple.JSONObject;//newly added
 
-public class perpetualTokenClient 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class perpetualTokenClient implements RequestHandler<RequestClass, ResponseClass>
 {
 	private static InputStream in;
-	   
-    public static void main(String[] args)
+	
+	public static void main(String[] args) {
+		String token = "gVijK1YarW//cwre/uIy4ZS+ghpXja5AivOSDCIvCKdv1ZmtkL71ITrPOSGzPy0IjG+AQS9IQq6txyJ/dah6ND1GEvoCVXxVHgqMwyLVm3TSZ7P9WNdDfDEwPm3GnPdG+l/E3c+3fxyECT6yNlW7lSWpESn3jj4E+UuPlJ97yCI=";
+        String operation = "Generate";
+        String apiUrl = "https://beta.leaseaccelerator.com/lease_accelerator/api/LeaseAccelerator/";
+        String requestXML="<APIRequest><Request><RequestId>1111</RequestId></Request><Payload><Report><ReportName>LedgerExport</ReportName><Format>XML</Format><Parameters><Parameter><Name>P_STARTING_FISCALYEAR</Name><Value>-100</Value></Parameter><Parameter><Name>P_PERIODDATE</Name><Value>-10</Value></Parameter><Parameter><Name>P_LOOKFORWARD</Name><Value>1</Value></Parameter><Parameter><Name>P_DETAILLEVEL</Name><Value>SCHEDULE</Value></Parameter><Parameter><Name>P_CALENDARTYPE</Name><Value /></Parameter><Parameter><Name>P_LEDGER</Name><Value>21</Value></Parameter><Parameter><Name>P_ENTITY</Name><Value>ACME MARKETS INC</Value></Parameter><Parameter><Name>P_EXCLUDENEWENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDETRANSFERREDENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDEPOSTEDENTRIES</Name><Value>N</Value></Parameter></Parameters></Report></Payload></APIRequest>";
+		RequestClass request=new RequestClass(token,operation,apiUrl,requestXML);
+		ResponseClass response=new perpetualTokenClient().handleRequest(request, null);
+		System.out.println("This is the status code");
+		System.out.println(response.statusCode);
+		System.out.println("This is the response XML");
+		System.out.println(response.body);
+	}
+	
+    public ResponseClass handleRequest(RequestClass request, Context context)
     {
     	Integer debug = 1;
              
@@ -45,14 +60,17 @@ public class perpetualTokenClient
             System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http","DEBUG");
             System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire","DEBUG");
         }
-        String token = "gVijK1YarW//cwre/uIy4ZS+ghpXja5AivOSDCIvCKdv1ZmtkL71ITrPOSGzPy0IjG+AQS9IQq6txyJ/dah6ND1GEvoCVXxVHgqMwyLVm3TSZ7P9WNdDfDEwPm3GnPdG+l/E3c+3fxyECT6yNlW7lSWpESn3jj4E+UuPlJ97yCI=";
-        String operation = "Generate";
+        //String token = "gVijK1YarW//cwre/uIy4ZS+ghpXja5AivOSDCIvCKdv1ZmtkL71ITrPOSGzPy0IjG+AQS9IQq6txyJ/dah6ND1GEvoCVXxVHgqMwyLVm3TSZ7P9WNdDfDEwPm3GnPdG+l/E3c+3fxyECT6yNlW7lSWpESn3jj4E+UuPlJ97yCI=";
+        //String operation = "Generate";
         //String xmlRequestFile = "test.xml";
-        String apiUrl = "https://beta.leaseaccelerator.com/lease_accelerator/api/LeaseAccelerator/";
+        //String apiUrl = "https://beta.leaseaccelerator.com/lease_accelerator/api/LeaseAccelerator/";
+        String token=request.token;
+        String operation=request.operation;
+        String apiUrl=request.apiUrl;
+        String requestXML=request.requestXML;
+        
         try
         {
-            if (token != null)
-            {
                 if (debug == 1)
                 {
                     System.out.println("Token Data  :" + token);
@@ -63,11 +81,11 @@ public class perpetualTokenClient
                
                 MultipartEntityBuilder builder = MultipartEntityBuilder.create();
                 builder.addPart("token", new StringBody(token, ContentType.DEFAULT_TEXT));
-                builder.addPart("requestId", new StringBody("12345", ContentType.DEFAULT_TEXT));
+                //builder.addPart("requestId", new StringBody("12345", ContentType.DEFAULT_TEXT));
                
                 //File requestFile = new File(xmlRequestFile);
                 //builder.addPart("file", new FileBody(requestFile.getAbsoluteFile()));
-                String requestXML="<APIRequest><Request><RequestId>1111</RequestId></Request><Payload><Report><ReportName>LedgerExport</ReportName><Format>XML</Format><Parameters><Parameter><Name>P_STARTING_FISCALYEAR</Name><Value>-100</Value></Parameter><Parameter><Name>P_PERIODDATE</Name><Value>-10</Value></Parameter><Parameter><Name>P_LOOKFORWARD</Name><Value>1</Value></Parameter><Parameter><Name>P_DETAILLEVEL</Name><Value>SCHEDULE</Value></Parameter><Parameter><Name>P_CALENDARTYPE</Name><Value /></Parameter><Parameter><Name>P_LEDGER</Name><Value>21</Value></Parameter><Parameter><Name>P_ENTITY</Name><Value>ACME MARKETS INC</Value></Parameter><Parameter><Name>P_EXCLUDENEWENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDETRANSFERREDENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDEPOSTEDENTRIES</Name><Value>N</Value></Parameter></Parameters></Report></Payload></APIRequest>";
+                //String requestXML="<APIRequest><Request><RequestId>1111</RequestId></Request><Payload><Report><ReportName>LedgerExport</ReportName><Format>XML</Format><Parameters><Parameter><Name>P_STARTING_FISCALYEAR</Name><Value>-100</Value></Parameter><Parameter><Name>P_PERIODDATE</Name><Value>-10</Value></Parameter><Parameter><Name>P_LOOKFORWARD</Name><Value>1</Value></Parameter><Parameter><Name>P_DETAILLEVEL</Name><Value>SCHEDULE</Value></Parameter><Parameter><Name>P_CALENDARTYPE</Name><Value /></Parameter><Parameter><Name>P_LEDGER</Name><Value>21</Value></Parameter><Parameter><Name>P_ENTITY</Name><Value>ACME MARKETS INC</Value></Parameter><Parameter><Name>P_EXCLUDENEWENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDETRANSFERREDENTRIES</Name><Value>N</Value></Parameter><Parameter><Name>P_EXCLUDEPOSTEDENTRIES</Name><Value>N</Value></Parameter></Parameters></Report></Payload></APIRequest>";
                 byte[] requestBytes=requestXML.getBytes(StandardCharsets.UTF_8);
                 builder.addPart("file",new ByteArrayBody(requestBytes,"test"));
                 
@@ -88,25 +106,38 @@ public class perpetualTokenClient
                     in = responseFromPost.getEntity().getContent();
                     String body = IOUtils.toString(in, "UTF-8");
                     System.out.println(body);
+                    ResponseClass resp = new ResponseClass();
+                    resp.setStatusCode(200);
+                    resp.setBody(body);
+                    return resp;
+                    
                 }
                 catch (ClientProtocolException e)
                 {
                     System.out.println(e);
+                    ResponseClass resp = new ResponseClass();
+                    resp.setStatusCode(400);
+                    resp.setBody("ClientProtocolException");
+                    return resp;
+
                 }
                 catch (IOException e)
                 {
                     System.out.println(e);
+                    ResponseClass resp = new ResponseClass();
+                    resp.setStatusCode(500);
+                    resp.setBody("IOException");
+                    return resp;
                 }
-            }  
                 
         }
         catch (Exception nsae)
         {
             System.out.println("NoSuchAlgorithmException in InvokeAPIFromFile: " + nsae.getLocalizedMessage());
-        }
-        finally
-        {
-            //end
+            ResponseClass resp = new ResponseClass();
+            resp.setStatusCode(400);
+            resp.setBody("NoSuchAlgorithmException");
+            return resp;
         }
     }
    
